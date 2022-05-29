36d153e2c40065f3df69262b57c0d08085fe3520
Ruth <mail@ruth-kapzan.de>
2022-05-29 21:25:19 +0200

    Eigenschaften des Graphen 1

--- src/app/classes/diagram/tsmodel.ts
+++ src/app/classes/diagram/tsmodel.ts
@@ -1,14 +1,23 @@
 import {TsNode} from "./tsnode";
 import {TsEdge} from "./tsedge";
+import {TsElement} from "./tselement";
+import {newArray} from "@angular/compiler/src/util";
 
 export class TsModel {
 
     private readonly _nodes: Array<TsNode>;
     private readonly _edges: Array<TsEdge>;
+    private _deadlocks: Array<TsNode>;
+    private _mortalTransitions: Array<TsEdge>;
+    private _cycleElements: Array<TsElement>;
+    //private _acyclic: boolean;
 
     constructor() {
         this._nodes = new Array<TsNode>();
         this._edges = new Array<TsEdge>();
+        this._deadlocks = new Array<TsNode>();
+        this._mortalTransitions = new Array<TsEdge>();
+        this._cycleElements = new Array<TsElement>();
     }
 
     get nodes(): Array<TsNode>{
@@ -24,6 +33,26 @@ export class TsModel {
         this._nodes.forEach(n =>
             {if (id == n.id) {return n}});
         return undefined;
+    }
+
+    public searchDeadlocks(): void{
+        let noDeadlock = new Array<TsNode>();
+        this._edges.forEach(e =>
+        noDeadlock.push(e.nodeFrom));
+        this._nodes.forEach(n =>
+            {if (! noDeadlock.includes(n))
+            {this._deadlocks.push(n)}}
+        )
+    }
+
+    public isAcyclic(): boolean{
+        //create copy of nodes and edges of the model
+        const tempNodes = [...this._nodes];
+        const tempEdges = [...this._edges];
+        //while (tempNodes.hasDeadlock())
+
+        return true;
+
     }
 
 
