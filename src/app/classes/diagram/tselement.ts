812fa73cedb57a83ee6bf4fd9c0a8c3ee1504f69
Ruth <mail@ruth-kapzan.de>
2022-06-03 11:33:36 +0200

    TRAN-56

--- src/app/classes/diagram/tselement.ts
+++ src/app/classes/diagram/tselement.ts
@@ -1,14 +1,56 @@
-import {Element} from './element';
+export abstract class TsElement {
 
-export class TsElement extends Element {
-
+    private _x: number;
+    private _y: number;
+    private _svgElement: SVGElement | undefined;
     private _id: string;
     protected readonly _label: string;
 
-    constructor(id: string, label: string) {
-        super();
+    protected constructor(id: string, label: string) {
+        this._x = 0;
+        this._y = 0;
         this._id = id;
         this._label = label;
+    }
+
+    get x(): number {
+        return this._x;
+    }
+
+    set x(value: number) {
+        this._x = value;
+    }
+
+    get y(): number {
+        return this._y;
+    }
+
+    set y(value: number) {
+        this._y = value;
+    }
+
+    public registerSvg(svg: SVGElement) {
+        this._svgElement = svg;
+        this._svgElement.onmousedown = (event) => {
+            this.processMouseDown(event);
+        };
+        this._svgElement.onmouseup = (event) => {
+            this.processMouseUp(event);
+        };
+    }
+
+    private processMouseDown(event: MouseEvent) {
+        if (this._svgElement === undefined) {
+            return;
+        }
+        this._svgElement.setAttribute('fill', 'red');
+    }
+
+    private processMouseUp(event: MouseEvent) {
+        if (this._svgElement === undefined) {
+            return;
+        }
+        this._svgElement.setAttribute('fill', 'black');
     }
 
     get id(): string {
