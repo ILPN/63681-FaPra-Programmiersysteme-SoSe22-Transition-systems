26f3a9e037357868550a240ed0a812133dcf06fc
Peter Guetersloh <peter.guetersloh@cgi.com>
2022-06-07 21:14:35 +0200

    Renamed models-ts to point.ts and add several unit-tests.

--- src/app/classes/spring_embedder/algorithmn.ts
+++ src/app/classes/spring_embedder/algorithmn.ts
@@ -1,4 +1,4 @@
-import {Node, Point, Graph} from './models'
+import {Point} from './point'
 import {Vector} from './vector'
 
 /**
@@ -16,7 +16,7 @@ type Cooling = (interation: number) => number;
 const defaultCooling: Cooling = (interation: number) => 1;
 
 
-class BaseSpringEmbedder {
+export class BaseSpringEmbedder {
     nodes: Point[];
 
     edges: Vector[];
@@ -71,29 +71,40 @@ class BaseSpringEmbedder {
     private embedd(maxIterations: number, eps: number): void {
         // The positions of the nodes. The initial positons are choosen
         // randomly.
+        console.log('Computing random positions');
         const positions = this.computeInitialPositions();
         // The forces moving the nodes
         const forces: Vector[] = [];
 
         let iteration = 1;
         while (iteration < maxIterations && this.normIsToHigh(forces, eps)) {
+            console.log(`${iteration}. iteration,`)
+            //this.printForces(forces);
             let index = 0;
             for (const position of positions) {
                 let force = new Vector(0, 0);
                 for (const p of positions) {
-                    // Compute the repuslive forces
-                    let repulsiveForce = this.computeRepulsiveForce(position, p);
-                    force.add(repulsiveForce);
-                    // Compute the attractive forces
-                    let attractiveForce = this.computeAttractiveForce(position, p);
-                    force.add(attractiveForce);
-                    // Keep the force for later use
-                    forces[index] = force;
-                    index++;
+                    if (position.equals(p)) {
+                        // Skip the case that both points are the same.
+                        forces[index] = force;
+                        index++;
+                    }
+                    else {
+                        // Compute the repuslive forces
+                        let repulsiveForce = this.computeRepulsiveForce(position, p);
+                        console.log(`repulsiveForce: x: ${repulsiveForce.x}, y: ${repulsiveForce.y}`);
+                        force.add(repulsiveForce);
+                        // Compute the attractive forces
+                        let attractiveForce = this.computeAttractiveForce(position, p);
+                        force.add(attractiveForce);
+                        // Keep the force for later use
+                        forces[index] = force;
+                        index++;
+                    }
                 }
-
             }
-
+            // TODO: This part needs to loop over the edges, not the nodes.
+            //       How to get all nodes here, that needs to be fetched.
             // Move the positions by the displaxement vectors.
             index = 0;
             for (const position of positions) {
@@ -107,7 +118,7 @@ class BaseSpringEmbedder {
             }
             iteration++;
         }
-
+        console.log(`Computed embedding after ${iteration} iterations`);
     }
 
     /**
@@ -124,6 +135,12 @@ class BaseSpringEmbedder {
      * Computes the repulsive force between the two given points.
      */
     private computeRepulsiveForce(point1: Point, point2: Point): Vector {
+        // If both points are the same we would face some computation erros
+        // and the result would be NaN. To avoid this we need to catch this
+        // case before.
+        if (point1.equals(point2)) {
+            return new Vector(0, 0);
+        }
         const repulsiveVector = Vector.byPoints(point1, point2);
         repulsiveVector.normalize();
         const scalar = Math.pow(this.idealSpringLength, 2) / (
@@ -150,10 +167,12 @@ class BaseSpringEmbedder {
      * Returns the maximal norm from an array of vectors
      */
     getMaxNorm(array: Array<Vector>): number {
-        if (array.length == 0) {
-            return -1;
+        if (array.length <= 0) {
+            throw new Error('Array must not be empty');
         }
-        return 0;
+        // Find the maximal norm on array.
+        const norms = array.map(vector => vector.norm());
+        return Math.max(...norms);
     }
 
     /**
@@ -166,9 +185,19 @@ class BaseSpringEmbedder {
         for (const node of this.nodes) {
             positions.push(Point.random());
         }
+        console.log(`Computed ${positions.length} random positions`)
         return positions;
     }
 
+    /**
+     * Delete me after debugging
+    */
+    printForces(forces: Array<Vector>): void {
+        console.log(`Printing ${forces.length} forces`)
+        for (const vec of forces) {
+            console.log(`vector: x: ${vec.x}, y: ${vec.y}`)
+        }
+    }
 }
 
 
--- src/app/classes/spring_embedder/vector_test.spec.ts
+++ src/app/classes/spring_embedder/vector_test.spec.ts
@@ -1,4 +1,4 @@
-import { Point } from './models';
+import { Point } from './point';
 import {Vector} from './vector';
 
 describe('Vector', () => {
