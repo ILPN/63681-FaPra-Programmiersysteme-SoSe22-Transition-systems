da5a33276725bc11deb02e63dfacdcd807b71100
Ruth <mail@ruth-kapzan.de>
2022-06-17 18:03:25 +0200

    TRAN-57 Testbeispiel verbessert

--- src/app/classes/diagram/properties_test.spec.ts
+++ src/app/classes/diagram/properties_test.spec.ts
@@ -12,18 +12,22 @@ describe('Test of getGraphProperties function with an example model',
         let node2 = new TsNode('n2', 'node2');
         let node3 = new TsNode('n3', 'node3');
         let node4 = new TsNode('n4', 'node4');
+        let node5 = new TsNode('n5','node5');
         let edge1 = new TsEdge('e1', 'edge1', 1, node1, node2);
         let edge2 = new TsEdge('e2', 'edge2', 1, node2, node3);
-        let edge3 = new TsEdge('e3', 'edge3', 1, node3, node1);
-        let edge4 = new TsEdge('e4', 'edge4', 1, node3, node4);
+        let edge3 = new TsEdge('e3', 'edge3', 1, node3, node4);
+        let edge4 = new TsEdge('e4', 'edge4', 1, node3, node5);
+        let edge5 = new TsEdge('e4', 'edge4', 1, node4, node2);
         myModel.addNode(node1);
         myModel.addNode(node2);
         myModel.addNode(node3);
         myModel.addNode(node4);
+        myModel.addNode(node5);
         myModel.addEdge(edge1);
         myModel.addEdge(edge2);
         myModel.addEdge(edge3);
         myModel.addEdge(edge4);
+        myModel.addEdge(edge5);
 
         // get properties of example model
         let myProperties = myModel.getGraphProperties();
