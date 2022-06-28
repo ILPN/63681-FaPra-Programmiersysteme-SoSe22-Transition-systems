13833ddcdb5faef3ab588b9e71ac2170118850b9
Samuel Eck <samuel.eck@posteo.de>
2022-06-28 08:31:01 +0200

    Validator and Parser Tests

--- src/app/services/parser.service.spec.ts
+++ src/app/services/parser.service.spec.ts
@@ -12,4 +12,25 @@ describe('ParserService', () => {
     it('should be created', () => {
         expect(service).toBeTruthy();
     });
+    it('Standard File Model', () => {
+        let file =`.type ts
+    .nodes
+n1 (10000)
+n2 (01000)
+n3 (00100)
+n4 (00001)
+n5 (00010)
+    .edges
+e1 t1 1 n1 n2
+e2 t2 1 n2 n3
+e3 t3 1 n3 n4
+e4 t4 1 n3 n5
+e5 t5 1 n4 n2
+`;
+        let model = service.parse(file);
+        expect(model.nodes.length).toBe(5);
+        expect(model.edges.length).toBe(5);
+        expect(model.getNode("n1")).toBeTruthy();
+        expect(model.getNode("fsdfs")).toBeFalsy();
+    });
 });
--- src/app/services/validator.service.spec.ts
+++ src/app/services/validator.service.spec.ts
@@ -1,16 +1,45 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { ValidatorService } from './validator.service';
+import {ValidatorService} from './validator.service';
 
 describe('ValidatorService', () => {
-  let service: ValidatorService;
+    let service: ValidatorService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ValidatorService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(ValidatorService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('Valid File should be valid', () => {
+        let file =`.type ts
+    .nodes
+n1 (10000)
+n2 (01000)
+n3 (00100)
+n4 (00001)
+n5 (00010)
+    .edges
+e1 t1 1 n1 n2
+e2 t2 1 n2 n3
+e3 t3 1 n3 n4
+e4 t4 1 n3 n5
+e5 t5 1 n4 n2
+`;
+        expect(service.validateTS(file)).toBeTrue();
+    });
+    it('missing node id in edges should be invalid', () => {
+        let file =`.type ts
+    .nodes
+n1 (10000)
+n2 (01000)
+
+    .edges
+e1 t1 1 n1 n3
+`;
+        expect(service.validateTS(file)).toBeFalse();
+    });
 });
