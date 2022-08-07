c1393a03c050774eb619e8c6e6c74962400ed5e0
wellar <NoStaticMembersininnerClasses>
2022-08-07 16:43:51 +0200

    Spring-Embedder Verbesserung

--- src/app/classes/spring_embedder/frspring-embedder1.spec.ts
+++ src/app/classes/spring_embedder/frspring-embedder1.spec.ts
@@ -1,7 +1,9 @@
-import { FRSpringEmbedder1 } from './frspring-embedder1';
+import {FRSpringEmbedder1} from './frspring-embedder1';
+import {TsModel} from "../diagram/tsmodel";
 
 describe('FRSpringEmbedder1', () => {
-  it('should create an instance', () => {
-    expect(new FRSpringEmbedder1()).toBeTruthy();
-  });
+    let model: TsModel;
+    it('should create an instance', () => {
+        expect(new FRSpringEmbedder1(model)).toBeTruthy();
+    });
 });
