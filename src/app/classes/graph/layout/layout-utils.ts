2d5390ab6102955e755cc2e2896cfbf940173ba9
wellar <NoStaticMembersininnerClasses>
2022-08-22 18:05:56 +0200

    TRAN-136 - Auf kleinen Bildschirmen liegt der Spring-Embedder-Graph außerhalb der Zeichen-Breite

--- src/app/classes/graph/layout/layout-utils.ts
+++ src/app/classes/graph/layout/layout-utils.ts
@@ -1,17 +1,8 @@
 import {SVGElementWithLabel} from "../../diagram/SVGElementWithLabel";
 import {TsNode} from "../../diagram/tsnode";
+import {DisplayComponent} from "../../../components/display/display.component";
 
 export class LayoutUtils {
-
-    //TODO Sollte nur an einer Stelle im Programm festgelegt sein
-    public static DRAWING_AREA_WIDTH_IN_PIXELS: number = window.innerWidth / 1.1;
-    //TODO Sollte nur an einer Stelle im Programm festgelegt sein
-    public static DRAWING_AREA_HEIGHT_IN_PIXELS: number = 400;
-
-    // Limits the position of nodes to the frame
-    private static WIDTH_POSITION_LIMIT: number = LayoutUtils.DRAWING_AREA_WIDTH_IN_PIXELS - SVGElementWithLabel.circleRadius;
-    // Limits the position of nodes to the frame
-    private static HEIGHT_POSITION_LIMIT: number = LayoutUtils.DRAWING_AREA_HEIGHT_IN_PIXELS - SVGElementWithLabel.circleRadius;
 
     /**
      * Adjusts the position of the passed node if its actual position would lie outside the drawing-area. The new position
@@ -20,8 +11,33 @@ export class LayoutUtils {
      */
     public static limitPositionToDrawingArea(node: TsNode){
         // Ist der Knoten-Radius in Pixeln definiert so wie die Zeichenebene?
-        let xPos: number = Math.min(Math.max(node.x, SVGElementWithLabel.circleRadius), (LayoutUtils.WIDTH_POSITION_LIMIT));
-        let yPos: number = Math.min(Math.max(node.y, SVGElementWithLabel.circleRadius), (LayoutUtils.HEIGHT_POSITION_LIMIT));
+        let xPos: number = Math.min(Math.max(node.x, SVGElementWithLabel.circleRadius), LayoutUtils.getWidthPositionLimitPx());
+        let yPos: number = Math.min(Math.max(node.y, SVGElementWithLabel.circleRadius), LayoutUtils.getHeightPositionLimitPx());
         node.setPosition(xPos, yPos);
     }
+
+    public static getWidthPositionLimitPx(): number {
+        return LayoutUtils.getDrawingAreaWidthPx() - SVGElementWithLabel.circleRadius;
+    }
+
+    public static getHeightPositionLimitPx(): number {
+        return LayoutUtils.getDrawingAreaHeightPx() - SVGElementWithLabel.circleRadius;
+    }
+
+    public static getDrawingAreaWidthPx(): number {
+        let xSize = document.getElementById(DisplayComponent.DRAWING_AREA_ID)?.clientWidth;
+        if(!xSize){
+            xSize = DisplayComponent.DRAWING_AREA_WIDTH_PX_FALLBACK_VALUE;
+        }
+        return xSize;
+    }
+
+    public static getDrawingAreaHeightPx(): number {
+        let ySize = document.getElementById(DisplayComponent.DRAWING_AREA_ID)?.clientHeight;
+        if(!ySize){
+            ySize = DisplayComponent.DRAWING_AREA_HEIGHT_PX_FALLBACK_VALUE;
+        }
+        return ySize;
+    }
+
 }
