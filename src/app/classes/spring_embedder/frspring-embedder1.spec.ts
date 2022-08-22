b69248e25e9465e7daf55d945e8fe95caa4c665a
wellar <NoStaticMembersininnerClasses>
2022-08-22 18:04:38 +0200

    Remove-Refactoring

--- src/app/classes/spring_embedder/frspring-embedder1.spec.ts
+++
@@ -1,9 +0,0 @@
-import {FRSpringEmbedder1} from './frspring-embedder1';
-import {TsModel} from "../diagram/tsmodel";
-
-describe('FRSpringEmbedder1', () => {
-    let model: TsModel;
-    it('should create an instance', () => {
-        expect(new FRSpringEmbedder1(model)).toBeTruthy();
-    });
-});
